227e80d170155fa6cb594c416c83bf732379d5b7
Gustavo Valente <159027272+gustavovvbs@users.noreply.github.com>
2024-09-20 19:13:46 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from psychopy import core, visual 
-
-win = visual.Window(fullscr=True)
-
-fixation = visual.ImageStim(win, image = '/Users/saladeux/Documents/WGaze-experiments/EXP/Stimuli/fixation.png')
-
-fixation.draw()
-win.flip()
-core.wait(3)
